81b94d5fd55d14258b40064fec2413d826a8ab2f
Hubert Lin <linhub15@gmail.com>
2019-05-07 17:30:30 -0600

    working require('http') and get

--- dataController.js
+++ dataController.js
@@ -1,6 +1,5 @@
 'use strict';
 let http = require('http');
-const url = 'http://api.morningstar.com/service/mf/Price/Mstarid/F00000NNHK?callback=?&format=json&username=morningstar&password=ForDebug&startdate=2008-01-01&enddate=2099-01-01'
 
 exports.list_all_data = (req, res) => {
   http.get(url, response => {
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import { IncomingMessage } from "http";
+
+let http = require('http');
 const url = 'http://api.morningstar.com/service/mf/Price/Mstarid/F00000NNHK?callback=?&format=json&username=morningstar&password=ForDebug&startdate=2008-01-01&enddate=2099-01-01'
-console.log(url);
+
+http.get(url, (message: IncomingMessage) => {
+  let body = '';
+  message.on('data', (data: string) => { return body += data ;} );
+  message.on('end', () => {
+    body = body.substring(2, body.length - 1);
+    body = JSON.parse(body).data.Prices;
+    console.log(body);
+  });
+});
